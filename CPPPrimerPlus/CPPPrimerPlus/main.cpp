2be913b700739b09f6e1c7cf8e75da5c266fe84a
Muhammad Shahrom Ali <estineali@gmail.com>
2019-08-14 16:58:14 +0500

    StringsInCPP
    All strings implementations in cpp from char to char array to const char array.

--- CPPPrimerPlus/CPPPrimerPlus/main.cpp
+++ CPPPrimerPlus/CPPPrimerPlus/main.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include "AllStrings.cpp"
 
+//Main
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+	AllStrings testingStrings;
+	testingStrings.GO();
 	return 0;
 }
